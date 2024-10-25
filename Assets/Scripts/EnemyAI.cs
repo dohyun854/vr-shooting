e9f9b244169dc8673294463c149d334ec51ff247
Dohyun Kim <61502073+dohyun854@users.noreply.github.com>
2024-10-25 14:15:55 +0900

    Add enemy & Update gitignore
    2024.10.25

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player; // 플레이어 오브젝트
+    public float detectionRange = 10f;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRange)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
