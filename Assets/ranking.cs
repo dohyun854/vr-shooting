0bb455ccf521fcb6b2bb9bb554fccc5c81990300
dav2d11 <yan1984asd@gmail.com>
2024-11-29 13:57:06 +0900

    Handling

--- Assets/ranking.cs
+++ Assets/ranking.cs
@@ -2,48 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ranking : MonoBehaviour
+public class Ranking : MonoBehaviour
 {
     private float[] bestScore = new float[5];
     private string[] bestName = new string[5];
+
     // Start is called before the first frame update
-    void Start(float currentScore, string currentName)
+    void Start()
     {
+        // Sample call to UpdateRanking for testing
+        UpdateRanking(100f, "Player1");
+    }
+
+    public void UpdateRanking(float currentScore, string currentName)
+    {
+        // Save the current player's score and name
         PlayerPrefs.SetString("CurrentPlayerName", currentName);
-        PlayerPrefs.SetFloat("CurrentPlayerScore",currentScore);
+        PlayerPrefs.SetFloat("CurrentPlayerScore", currentScore);
 
-        flaot tmpScore = 0f;
-        string tmpName = "";
-        for (int i=0;i<5;i++)
+        float tmpScore;
+        string tmpName;
+
+        for (int i = 0; i < 5; i++)
         {
-            bestscore[i] = PlayerPrefs.GetFloat(i + "BestScore");
-            bestName[i] = PlayerPrefs.GetFloat(i + "BestName");
+            // Load existing scores and names
+            bestScore[i] = PlayerPrefs.GetFloat(i + "BestScore", 0f);
+            bestName[i] = PlayerPrefs.GetString(i + "BestName", "");
 
-            while (bestScore[i]<currentScore)
+            // Update ranking if the current score is higher
+            if (bestScore[i] < currentScore)
             {
-                tmpNameScore = bestScore[i];
+                tmpScore = bestScore[i];
                 tmpName = bestName[i];
+
                 bestScore[i] = currentScore;
                 bestName[i] = currentName;
 
                 PlayerPrefs.SetFloat(i + "BestScore", currentScore);
-                PlayerPrefs.SetString(i.Tostring() + "BestName", currentName);
+                PlayerPrefs.SetString(i.ToString() + "BestName", currentName);
 
-                currentScore= tmpScore;
+                currentScore = tmpScore;
                 currentName = tmpName;
             }
         }
-        for (int i=0;i<5;i++)
-        {
-            PlayerPrefs.SetFloat(i+ "BestScore", bestScore[i]);
-            PlayerPrefs.SetString(i.ToString() + "BestName", bestName[i]);
 
+        // Save updated scores and names
+        for (int i = 0; i < 5; i++)
+        {
+            PlayerPrefs.SetFloat(i + "BestScore", bestScore[i]);
+            PlayerPrefs.SetString(i.ToString() + "BestName", bestName[i]);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Optional: Add logic for real-time updates if needed
     }
 }
