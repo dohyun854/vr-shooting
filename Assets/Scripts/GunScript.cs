90b6d204e0d4709054d6cdcac0fef01130a48139
dav2d11 <yan1984asd@gmail.com>
2024-12-23 18:45:39 +0900

    Playscene shooting

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -9,30 +9,39 @@ public class GunScript : MonoBehaviour
     public Transform firePoint;     // 총구 위치
     public float bulletSpeed = 20f; // 총알 속도
 
-    private InputAction fireAction; // Fire 액션
+    private InputAction fireActionRight; // Fire 액션
+    private InputAction fireActionLeft;
 
-    private void OnEnable()
-    {
-        // Fire 액션 활성화
-        fireAction = new InputAction(type: InputActionType.Button, binding: "<XRController>{RightHand}/triggerPressed");
-        fireAction.performed += _ => Shoot();
-        fireAction.Enable();
-    }
+   private void OnEnable()
+{
+    // 오른손 트리거로 총 쏘기
+    fireActionRight = new InputAction(type: InputActionType.Button, binding: "<XRController>{RightHand}/triggerPressed");
+    fireActionRight.performed += _ => Shoot();
+    fireActionRight.Enable();
 
-    private void OnDisable()
-    {
-        // Fire 액션 비활성화
-        fireAction.Disable();
-    }
+    // 왼손 트리거로 총 쏘기
+    fireActionLeft = new InputAction(type: InputActionType.Button, binding: "<XRController>{LeftHand}/triggerPressed");
+    fireActionLeft.performed += _ => Shoot();
+    fireActionLeft.Enable();
+}
 
-    private void Shoot()
+private void OnDisable()
+{
+    // 오른손 트리거 비활성화
+    fireActionRight.Disable();
+
+    // 왼손 트리거 비활성화
+    fireActionLeft.Disable();
+}
+
+private void Shoot()
+{
+    // 총알 생성 및 발사
+    GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    Rigidbody rb = bullet.GetComponent<Rigidbody>();
+    if (rb != null)
     {
-        // 총알 생성 및 발사
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.velocity = firePoint.forward * bulletSpeed;
-        }
+        rb.velocity = firePoint.forward * bulletSpeed;
     }
+}
 }
